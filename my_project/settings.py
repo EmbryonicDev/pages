87074002f7adf823ff4dea805d0c6e7d2ceead86
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-19 14:46:08 +0200

    Change settings to allow github to host site

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^82$utyd+e!d$se=0w_o03iet&6__()#*he08&dlvb(+v_f60m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["EmbryonicDev.github.io"]
 
 
 # Application definition
