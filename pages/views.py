651dab6762d03390e0909fef2335f55c72ea5f62
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-19 11:08:02 +0200

    Create class based view for pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+""" 
+TemplateView is a useful tool for rendering
+templates in Django and is suitable for simple
+use cases where data processing is not required.
+"""
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
